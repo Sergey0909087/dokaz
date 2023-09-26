713a3a1a68406fa8823e6b6a58761f7d122054f5
Sergey0909087 <128168133+Sergey0909087@users.noreply.github.com>
2023-09-26 12:15:09 +0300

    Add files via upload

---
+++ test_project/manage.py
@@ -0,0 +1,33 @@
+# deapozonik = int(input("Enter: "))
+# hi = int(input("Enter: "))
+# while deapozonik <= hi:
+#     print(deapozonik)
+#     deapozonik = deapozonik + 1
+
+# lolik = int(input("Enter num: "))
+# lolik2 = int(input())
+# while lolik <= lolik2:
+#     print(lolik)
+#     lolik += 1
+
+# ger = int(input("Введи первое: "))
+# gerik = int(input("введи второе: "))
+# while ger <= gerik:
+#     if ger % 2 == 1:
+#         print(ger)
+#     ger += 1
+
+# gercules = int(input("Введи первое: "))
+# gerakl = int(input("Введи второе: "))
+# while gercules <= gerakl:
+#     print(gerakl)
+#     gerakl -= 1
+
+# popcorn = int(input("Enter num: "))
+# vata = int(input("Enters num: "))
+# voda = min(popcorn, vata)
+# pepsi = max(popcorn, vata)
+# while vata <= pepsi:
+#     if voda % 2 == 1:
+#         print(voda)
+#     voda += 1
---
+++ test_project/new.py
@@ -0,0 +1,18 @@
+# print('123', end=' ')
+# print('456', end=' ')
+# print('789', end=' ')
+
+# deapozonik = int(input("Enter: "))
+# hi = int(input("Enter: "))
+# while deapozonik <= hi:
+#     print(deapozonik)
+#     deapozonik = deapozonik + 1
+
+# ger = int(input("Введи первое: "))
+# gerik = int(input("введи второе: "))
+# summa = 0
+# while ger <= gerik:
+#     print(ger)
+#     summa += ger
+#     ger += 1
+# print(summa)
